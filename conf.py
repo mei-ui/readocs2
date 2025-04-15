07c2d668670c1c867ba629c0d1e5ffa871f2bb00
mei-ui <64576991+mei-ui@users.noreply.github.com>
2025-04-15 15:20:07 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,81 +1,33 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
-
 project = 'InfoScan'
 copyright = '2021, Zilch'
 author = 'Zilch'
-
-# The full version, including alpha/beta/rc tags
 release = '0.1'
-import os
-output_dir = os.environ.get('READTHEDOCS_OUTPUT', '_build')  # 默认兼容本地构建
-html_dir = os.path.join(output_dir, 'html')
 
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ['myst_parser'
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.imgmath',
+    'sphinx_copybutton',
+    'sphinx.ext.viewcode'
 ]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_js_files = ['js/baidutongji.js']
 
-# For md files
 source_suffix = {
     '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
+    '.md': 'myst',
+    '.txt': 'myst'
 }
 
-# Add custom styles
+autodoc_mock_imports = ['myst_parser']
+needs_sphinx = '4.0'
+nitpicky = True
+nitpick_ignore = [('py:class', 'myst_parser.main.MdParserConfig')]
+
 def setup(app):
     app.add_css_file('css/custom-style.css')
-
-rst_prolog = '\n.. include:: .custom-style.rst\n'
-
-# Add custom js
-# for ip calculate
-html_js_files = [
-    'js/baidutongji.js'
-]
-
-
-# Add modules
-autodoc_mock_imports = ['myst_parser']
-
